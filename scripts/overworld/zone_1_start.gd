d8086f43fa208ec6d1c51267bb322254e4727527
JVPC <jeremellev@icloud.com>
2026-06-14 21:13:36 -0600

    added minimap

--- scripts/overworld/zone_1_start.gd
+++ scripts/overworld/zone_1_start.gd
@@ -11,8 +11,15 @@ func _ready() -> void:
 		var default_spawn = get_node("default_spawn")
 		NavigationManager.trigger_player_spawn(default_spawn.global_position, "down")
 
+	_setup_minimap.call_deferred()
+
 
 func _on_level_spawn(destination_tag: String):
 	var door_path = "doors/door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
+
+
+func _setup_minimap() -> void:
+	var minimap = get_node("minimap")
+	minimap.set_follow_target(NavigationManager._player_instance)
